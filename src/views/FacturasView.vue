f8283634621d8083205f1ac7f9a85dbd68bef7a2
XclusivePasasin <elizar.rebollo23@itca.edu.sv>
2024-10-07 19:09:09 -0600

    change desing invoices view

--- src/views/FacturasView.vue
+++ src/views/FacturasView.vue
@@ -5,7 +5,7 @@
     <div class="flex-1 flex flex-col overflow-hidden">
       <!-- Header Component -->
       <HeaderComponent />
-      <div class="container mx-auto p-6">
+      <div class="container mx-auto p-6 flex-1 flex flex-col overflow-y-auto">
         <div class="flex space-x-4 mb-6">
           <input
             v-model="query"
@@ -25,60 +25,187 @@
             placeholder="Hasta"
             class="w-50 px-4 py-2 rounded-full border focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
-          <button @click="fetchFacturas" class=" w-35 px-6 py-2 bg-blue-600 text-white rounded-full hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
+          <button
+            @click="fetchFacturas"
+            class="flex items-center w-35 px-6 py-2 bg-slate-600 text-white rounded-full font-semibold hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-filter mr-2"
+            >
+              <polygon points="22 3 2 3 10 12.46 10 19 14 21 14 12.46 22 3" />
+            </svg>
             Filtrar
           </button>
-          <button class="px-6 py-2 bg-blue-600 text-white rounded-full hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
+          <button
+            class="px-6 py-2 bg-slate-600 text-white rounded-full hover:bg-slate-700 font-semibold focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2"
+          >
             Procesar
           </button>
-          <button class=" px-6 py-2 bg-blue-600 text-white rounded-full hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
+          <button
+            class="flex px-6 py-2 bg-slate-600 text-white rounded-full font-semibold hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-800 focus:ring-offset-2"
+          >
             Descargar
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-download ml-2"
+            >
+              <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
+              <polyline points="7 10 12 15 17 10" />
+              <line x1="12" x2="12" y1="15" y2="3" />
+            </svg>
           </button>
         </div>
-  
-        <div class="overflow-x-auto">
-          <table class="w-full bg-white shadow-md rounded-lg overflow-hidden">
+
+        <!-- Tabla con scroll invisible -->
+        <div class="overflow-x-auto flex-grow scrollbar-none">
+          <table class="w-full bg-white shadow-md rounded-lg overflow-y-auto">
             <thead class="bg-gray-100 sticky top-0 z-10">
               <tr>
-                <!-- Column Headers -->
-                <th class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Nombre de emisor</th>
-                <th class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">NRC Emisor</th>
-                <th class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">NIT Emisor</th>
-                <th class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Fecha</th>
-                <th class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Número de control</th>
-                <th class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Código Generación</th>
-                <th class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Sello de Recepción</th>
-                <th class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Monto</th>
-                <th class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Procesar</th>
-                <th class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Acciones</th>
+                <th
+                  class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  Nombre de emisor
+                </th>
+                <th
+                  class="px-4 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  NRC Emisor
+                </th>
+                <th
+                  class="px-4 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  NIT Emisor
+                </th>
+                <th
+                  class="px-4 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  Fecha
+                </th>
+                <th
+                  class="px-4 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  Número de control
+                </th>
+                <th
+                  class="px-4 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  Código Generación
+                </th>
+                <th
+                  class="px-4 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  Sello de Recepción
+                </th>
+                <th
+                  class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  Monto
+                </th>
+                <th
+                  class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  Procesar
+                </th>
+                <th
+                  class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  Acciones
+                </th>
               </tr>
             </thead>
             <tbody class="divide-y divide-gray-200">
-              <!-- Table Rows -->
-              <tr v-for="(item, index) in tableData" :key="index" :class="index % 2 === 0 ? 'bg-white' : 'bg-gray-50'">
-                <td class="px-4 py-4 whitespace-nowrap text-sm text-gray-900">{{ item.nom_emisor }}</td>
-                <td class="px-4 py-4 whitespace-nowrap text-sm text-gray-900">{{ item.nrc_emisor }}</td>
-                <td class="px-4 py-4 whitespace-nowrap text-sm text-gray-900">{{ item.nit_emisor }}</td>
-                <td class="px-4 py-4 whitespace-nowrap text-sm text-gray-900">{{ item.fecha_emision }}</td>
-                <td class="px-4 py-4 whitespace-nowrap text-sm text-gray-900">{{ item.num_control }}</td>
-                <td class="px-4 py-4 whitespace-nowrap text-sm text-gray-900">{{ item.cod_gen }}</td>
-                <td class="px-4 py-4 whitespace-nowrap text-sm text-gray-900">{{ item.sello_recepcion }}</td>
-                <td class="px-4 py-4 whitespace-nowrap text-sm text-gray-900">{{ item.monto }}</td>
-                <td class="px-4 py-4 whitespace-nowrap text-sm text-gray-900">
-                  <input type="checkbox" class="form-checkbox h-5 w-5 text-blue-600" />
+              <tr
+                v-for="(item, index) in tableData"
+                :key="index"
+                :class="index % 2 === 0 ? 'bg-white' : 'bg-gray-50'"
+              >
+                <td class="px-4 py-4 w-48 break-words text-sm text-gray-900">
+                  {{ item.nom_emisor }}
                 </td>
-                <td class="px-4 py-4 whitespace-nowrap text-sm text-gray-900">
-                  <!-- Buttons for PDF and JSON -->
-                  <button class="px-3 py-1 bg-yellow-500 text-white rounded hover:bg-yellow-600 mr-2">
-                    Ver PDF
-                  </button>
-                  <button class="px-3 py-1 bg-gray-500 text-white rounded hover:bg-gray-600">
-                    Ver JSON
-                  </button>
+                <td class="px-4 py-4 w-40 break-words text-sm text-gray-900">
+                  {{ item.nrc_emisor }}
+                </td>
+                <td class="px-4 py-4 w-40 break-words text-sm text-gray-900">
+                  {{ item.nit_emisor }}
+                </td>
+                <td class="px-4 py-4 w-40 break-words text-sm text-gray-900">
+                  {{ item.fecha_emision }}
+                </td>
+                <td class="px-4 py-4 w-40 break-words text-sm text-gray-900">
+                  {{ item.num_control }}
+                </td>
+                <td class="px-4 py-4 w-50 break-words text-sm text-gray-900">
+                  {{ item.cod_gen }}
+                </td>
+                <td
+                  class="px-4 py-4 w-40 text-center text-sm text-gray-900 break-all overflow-wrap break-word"
+                >
+                  {{ item.sello_recepcion }}
+                </td>
+                <td class="px-4 py-4 w-30 break-words text-sm text-gray-900">
+                  {{ item.monto }}
+                </td>
+                <td class="px-4 py-4 text-sm text-gray-900">
+                  <input
+                    type="checkbox"
+                    class="form-checkbox h-5 w-5"
+                  />
+                </td>
+                <td class="px-4 py-4 text-sm text-gray-900">
+                  <div class="flex flex-col space-y-2">
+                    <button
+                      class="px-4 py-2 bg-sky-600 text-white rounded-full hover:bg-sky-700"
+                    >
+                      PDF
+                    </button>
+                    <button
+                      class="px-4 py-2 bg-gray-600 text-white rounded-full hover:bg-gray-700"
+                    >
+                      DTE
+                    </button>
+                  </div>
                 </td>
               </tr>
             </tbody>
           </table>
+        </div>
+
+        <!-- Pagination controls -->
+        <div class="flex justify-between items-center mt-4">
+          <button
+            :disabled="page === 1"
+            @click="prevPage"
+            class="px-4 py-2 bg-gray-300 text-gray-800 rounded-full hover:bg-gray-400 disabled:bg-gray-200"
+          >
+            Anterior
+          </button>
+          <div class="text-gray-800">
+            Página {{ page }} de {{ pagination.pages }}
+          </div>
+          <button
+            :disabled="page === pagination.pages"
+            @click="nextPage"
+            class="px-4 py-2 bg-gray-300 text-gray-800 rounded-full hover:bg-gray-400 disabled:bg-gray-200"
+          >
+            Siguiente
+          </button>
         </div>
       </div>
     </div>
@@ -86,16 +213,22 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted } from "vue";
 import SideBar from "../components/SideBar.vue";
 import HeaderComponent from "../components/HeaderComponent.vue";
-import InvoiceService from '../services/InvoiceService';  // Asegúrate de que tienes el servicio configurado
+import InvoiceService from "../services/InvoiceService"; // Asegúrate de que tienes el servicio configurado
 
 // Variables reactivas
-const query = ref('');
-const startDate = ref('');
-const endDate = ref('');
+const query = ref("");
+const startDate = ref("");
+const endDate = ref("");
 const tableData = ref([]);
+const page = ref(1); // Página actual
+const pagination = ref({
+  total: 0,
+  per_page: 15,
+  pages: 0,
+});
 
 // Función para obtener las facturas desde el backend
 const fetchFacturas = async () => {
@@ -104,10 +237,28 @@ const fetchFacturas = async () => {
       query: query.value,
       startDate: startDate.value,
       endDate: endDate.value,
+      page: page.value, // Enviar página actual
     });
     tableData.value = response.data.invoices;
+    pagination.value = response.data.pagination; // Actualiza la información de la paginación
   } catch (error) {
-    console.error('Error fetching facturas:', error);
+    console.error("Error fetching facturas:", error);
+  }
+};
+
+// Cambiar a la página anterior
+const prevPage = () => {
+  if (page.value > 1) {
+    page.value -= 1;
+    fetchFacturas();
+  }
+};
+
+// Cambiar a la página siguiente
+const nextPage = () => {
+  if (page.value < pagination.value.pages) {
+    page.value += 1;
+    fetchFacturas();
   }
 };
 
@@ -117,4 +268,13 @@ onMounted(() => {
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+.scrollbar-none::-webkit-scrollbar {
+  display: none;
+}
+.scrollbar-none {
+  -ms-overflow-style: none; /* IE y Edge */
+  scrollbar-width: none; /* Firefox */
+}
+
+</style>
